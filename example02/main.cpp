1a3e92193480d364d96dfa01fa59a2e754158f26
sandwich-fish <bluecurve2007@outlook.com>
2024-09-11 13:45:16 +0900

    add some changes in main.cpp.

--- example02/main.cpp
+++ example02/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
 #include <iterator>
-#include <set>
 #include <unordered_set>
 #include <list>
+#include <functional>
+#include <random>
 #include "assoiter.h"
 
 using namespace std;
+using namespace std::placeholders;
 
 int main() {
     unordered_set<int> coll;
@@ -28,6 +29,30 @@ int main() {
     vector<int> vals = {33, 67, -4, 13, 5, 2};
     copy(vals.begin(), vals.end(), asso_inserter(coll));
     copy(coll.cbegin(), coll.cend(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    // generate a random number
+    mt19937 rng;
+    generate_n(ostream_iterator<mt19937::result_type>(cout, " "), 5, ref(rng));
+    cout << endl;
+
+    list<int> coll1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << "coll1: " << endl;
+    copy(coll1.cbegin(), coll1.cend(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    list<int>::const_iterator pos;
+    // method1: lambda expression.
+//    pos = remove_if(coll1.begin(), coll1.end(), [](int val){return val == 3;});
+
+    // method2: std::bind and equal_to function object.
+    pos = remove_if(coll1.begin(), coll1.end(), bind(equal_to<int>(), _1, 3));
+
+    // method3: function object
+    // TODO:
+
+    cout << "3rd removed: " << endl;
+    copy(coll1.cbegin(), pos, ostream_iterator<int>(cout, " "));
     cout << endl;
     return 0;
 }
